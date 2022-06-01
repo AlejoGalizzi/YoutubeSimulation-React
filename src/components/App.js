66aecf7d2839e3fc8e2683080928422bdbc19017
Alejo Galizzi <alejogalizzi@gmail.com>
2022-06-01 16:16:00 -0300

    Axios request created in app and searchBar component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtubeRequest from "../api/youtube-request";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
+  state = {videos: []}
+
+  onTermSubmit = async (term) => {
+    const result = await youtubeRequest.get('/search', {
+                    params: {
+                      q: term
+                    }
+                  });
+    this.setState({videos: result.data.items});
+  }
+
   render() {
     return (
     <div className="ui container">
-      <SearchBar />  
+      <SearchBar onFormSubmit={this.onTermSubmit}/>
+      <VideoList videos={this.state.videos}/>  
     </div>
     );
   }
