546e37bbca88baae2b42785f9f7be8b599771bc6
Alejo Galizzi <alejogalizzi@gmail.com>
2022-06-01 21:50:48 -0300

    Add some functionality to the video's item.

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import VideoItem from './VideoItem';
+import './VideoItem.css';
 
-const VideoItem = () => {
-  return (<div>
-    
-  </div>);
-}
+const VideoItem = ({video}) => {
+  return (
+    <div className='video-item item'>
+      <div className='ui small image'>
+        <img src={video.snippet.thumbnails.medium.url}/>
+      </div>
+      <div className='middle aligned content'>
+        {video.snippet.title}
+      </div>
+
+    </div>
+  );
+};
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
 const VideoList = ({videos}) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem /> 
+    return <VideoItem video={video}/> 
   })
-  return (<div>
+  return (<div className='ui divided items'>
     {renderedList}
   </div>);
 }
