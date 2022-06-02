97d05abbe79b10c1c940b26fbe974e1e19126e5c
Alejo Galizzi <alejogalizzi@gmail.com>
2022-06-01 22:22:31 -0300

    Add method to set a selected video from the list of videos.

--- src/components/App.js
+++ src/components/App.js
@@ -4,31 +4,35 @@ import youtubeRequest from "../api/youtube-request";
 import VideoList from "./VideoList";
 
 class App extends React.Component {
-  state = {videos: []}
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
-    if(term === ""){
-      this.setState({videos: []})
-    }else{
-      const result = await youtubeRequest.get('/search', {
+    if (term === "") {
+      this.setState({ videos: [] });
+    } else {
+      const result = await youtubeRequest.get("/search", {
         params: {
-          q: term
-        }
+          q: term,
+        },
       });
-      this.setState({videos: result.data.items});
+      this.setState({ videos: result.data.items });
     }
-  }
+  };
 
-  onVideoSelected = (video) => {
-    console.log(`From the app! ${video}`);
-  }
+  onVideoSelect = video => {
+    console.log('From the app!', video);
+    this.setState({selectedVideo: video});
+  };
 
   render() {
     return (
     <div className="ui container">
-      <SearchBar onFormSubmit={this.onTermSubmit}/>
-      <VideoList videos={this.state.videos}/>  
-    </div>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
+      </div>
     );
   }
 }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import './VideoItem.css';
+import React from "react";
+import "./VideoItem.css";
 
-const VideoItem = ({video}) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className='video-item item'>
-      <div className='ui small image'>
-        <img src={video.snippet.thumbnails.medium.url} alt='item'/>
+    <div
+      onClick={() => {
+        onVideoSelect(video);
+      }}
+      className="video-item item"
+    >
+      <div className="ui small image">
+        <img src={video.snippet.thumbnails.medium.url} alt="item" />
       </div>
-      <div className='middle aligned content'>
-        {video.snippet.title}
-      </div>
-
+      <div className="middle aligned content">{video.snippet.title}</div>
     </div>
   );
 };
